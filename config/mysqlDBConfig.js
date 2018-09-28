d4c5df38a44ec4a87a32b9b20c0614647eac3b64
Javier Corona <fjavier.corona@hotmail.com>
2018-09-28 16:52:49 -0500

    update

--- config/mysqlDBConfig.js
+++ config/mysqlDBConfig.js
@@ -1,8 +1,8 @@
 // Fill the object with your own credentials
 
 module.exports = {
-	host: '',
-	user: '',
-	password: '',
-	database: '',
+	host: 'your_host',
+	user: 'your_user',
+	password: 'your_password',
+	database: 'your_database',
 };
