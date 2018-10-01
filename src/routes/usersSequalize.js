bc44b194a4a58fec2acfd3bcf36bd43db7a30401
Javier Corona <fjavier.corona@hotmail.com>
2018-10-01 10:26:28 -0500

    user sequalize update

--- src/routes/usersSequalize.js
+++ src/routes/usersSequalize.js
@@ -3,25 +3,37 @@ const router = express.Router();
 const sequelizeConnection = require('../dbconnections/sequelize/sequelizeConectionPool');
 
 // Get all users
-router.get('/users/sequelize', (req, res) => {});
+router.get('/users-sequelize', (req, res) => {
+	res.json({});
+});
 
 // Get an specific user
-router.get('/users/sequelize/:userId', (req, res) => {});
+router.get('/users-sequelize/:userId', (req, res) => {
+	res.json({});
+});
 
 // Add a user
-router.post('/users/sequelize', (req, res) => {});
+router.post('/users-sequelize', (req, res) => {
+	res.json({});
+});
 
 // Update a user
-router.put('/users/sequelize/:userId', (req, res) => {});
+router.put('/users-sequelize/:userId', (req, res) => {
+	res.json({});
+});
 
 // Delete a user - true deletion
 
 /*
-router.delete('/users/sequelize/:userId', (req, res) => {});
+router.delete('/users-sequelize/:userId', (req, res) => {
+	res.json({});
+});
 */
 
 // Delete a user - Just disable it
 
-router.delete('/users/sequelize/:userId', (req, res) => {});
+router.delete('/users-sequelize/:userId', (req, res) => {
+	res.json({});
+});
 
 module.exports = router;
