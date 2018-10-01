f6351ba5eeded03b0eadca8d8097e267602ee8a1
Javier Corona <fjavier.corona@hotmail.com>
2018-10-01 09:45:24 -0500

    dbconnections internal structure

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const mysqlConnection = require('../dbconnections/mysqlConnection');
+const mysqlConnection = require('../dbconnections/mysql/mysqlConnection');
 
 // Get all users
 router.get('/users', (req, res) => {
