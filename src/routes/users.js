54167cfbc7d49d5259875dd142b40eaf98d5f29b
Javier Corona <fjavier.corona@hotmail.com>
2018-10-01 09:40:38 -0500

    databasese.js => mysqlConnection.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const mysqlConnection = require('../dbconnections/database');
+const mysqlConnection = require('../dbconnections/mysqlConnection');
 
 // Get all users
 router.get('/users', (req, res) => {
